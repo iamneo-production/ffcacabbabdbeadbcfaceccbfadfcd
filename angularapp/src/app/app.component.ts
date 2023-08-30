d2f6d46487a6130d551a72c227c17972ead5894c
Iamneo <techadmin@iamneo.ai>
2023-08-30 12:52:52 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder } from "@angular/forms";
+//import { DOCUMENT } from "@angular/common";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  resValue: any
+  fromCurrency: any
+  toCurrency: any
+  amount:any
+  constructor(public fb: FormBuilder) { }
+  CurrencyForm = this.fb.group({
+    fromCurrency: [''],
+    amount: [''],
+    toCurrency: ['']
+  })
+  onSubmitt() {
+    this.amount=this.CurrencyForm.get('amount').value;  
+    this.fromCurrency=this.CurrencyForm.get('fromCurrency').value;
+    this.toCurrency=this.CurrencyForm.get('toCurrency').value;
+    //this.amount=
+    //this.resValue='1.00';
+    this.resValue=(this.toCurrency/this.fromCurrency)*this.amount;
+    this.resValue=Math.round(this.resValue);
+    this.resValue=parseFloat(this.resValue).toFixed(2);
+  }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,15 +1,21 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
